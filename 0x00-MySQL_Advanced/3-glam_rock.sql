36526f644c8576b728882e15bc62a2cf6878bd98
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-09 20:00:50 +0100

    store

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, 
+       CASE
+           WHEN formed > 0 AND split > 0 THEN (2022 - formed)
+           WHEN formed > 0 AND split = 0 THEN (2022 - formed)
+           ELSE 0
+       END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
+
