73e6fd748a999a3d81df997305a1c898429b20b6
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-09 21:20:39 +0100

    bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,13 @@
--- Définition de la procédure stockée
+-- Script that creates a stored procedure AddBonus
 CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score INTEGER
 )
 BEGIN
-    DECLARE project_id INT;
-
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
-    END IF;
-
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score);
-
-END
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;
