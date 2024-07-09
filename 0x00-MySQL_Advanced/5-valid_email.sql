26452c564c86669022a484a13422373f77002056
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-09 21:53:35 +0100

    valid_email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,10 @@
 -- Create the trigger
+DELIMITER $$ ;
 CREATE TRIGGER reset_valid_email AFTER UPDATE ON users
 FOR EACH ROW
+BEGIN
     IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
+END;$$
+delimiter ;
