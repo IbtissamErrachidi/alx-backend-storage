2802ac64fc74b28d84160a3c34328de63c0b1fc4
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-19 21:39:01 +0100

    redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+import redis
+import uuid
+#!/usr/bin/env python3
+"""
+Contains the class definition for redis cache
+"""
+class Cache:
+    """
+    Defines methods to handle redis cache operations
+    """
+    def __init__(self) -> None:
+        """
+        Initialize redis client
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in redis cache
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
