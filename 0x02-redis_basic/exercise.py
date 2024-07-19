e9ab6e1043edd85862636bada3ba0417fc677cee
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-19 23:10:42 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,64 @@
 #!/usr/bin/env python3
-import redis
-import uuid
-from typing import Union, Callable, Optional
-from functools import wraps
 """
 Contains the class definition for redis cache
 """
+import redis
+import uuid
+from functools import wraps
+from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Counts the number of times a function is called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for the decorated function
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Counts the number of times a function is called
+    """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for the decorated function
+        """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    Replays the history of a function
+    """
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
+
+
 class Cache:
     """
     Defines methods to handle redis cache operations
@@ -16,8 +69,9 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
- 
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in redis cache
@@ -26,7 +80,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-      def get(self, key: str, fn: Optional[Callable] = None)\
+    def get(self, key: str, fn: Optional[Callable] = None)\
             -> Union[str, bytes, int, float, None]:
         """
         Get data from redis cache
@@ -48,19 +102,3 @@ class Cache:
         Get data as integer from redis cache
         """
         data = self
-
-
-def count_calls(method: Callable) -> Callable:
-    """
-    Counts the number of times a function is called
-    """
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """
-        Wrapper function for the decorated function
-        """
-        key = method.__qualname__
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-
-    return wrapper
